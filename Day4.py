5bdacfd08ab55c0e1fd2c958f0381c7b4f9ec14c
carsten <carsten_schumaker@yahoo.com>
2018-12-12 21:11:35 -0500

    Finished Part 1 solution

--- Day4.py
+++ Day4.py
@@ -1,4 +1,6 @@
 from datetime import datetime, timedelta
+from collections import Counter
+
 
 with open('Day4Input.txt', 'r') as f:
     content = f.readlines()
@@ -36,28 +38,46 @@ for i in new_list:
 
 
 largest_v = timedelta(0, 0)
+most_min_guard = 0
 for k, v in dicty.items():
     if v > largest_v:
         largest_v = v
-        print(k)
-
-# some sort of set intersection may need to be done with timespans
-# for guard 2411
+        most_min_guard = int(k)
 
 
 def get_guard_intervals(guard):
     guard_intervals = []
-    last_guard = ''
+    last_guardd = ''
     for x in sorted(new_log_list):
         if "Guard" in x[1]:
-            last_guard = x[1].split("#")[1].split(" ")[0]
+            last_guardd = x[1].split("#")[1].split(" ")[0]
         else:
-            if last_guard == str(guard):
+            if last_guardd == str(guard):
                 guard_intervals.append(x)
 
     return guard_intervals
 
 
-intervals = get_guard_intervals(2411)
-print(intervals)
-print(sorted(new_log_list))
+intervals = get_guard_intervals(most_min_guard)
+intervals_datetimes = []
+for t in intervals:
+    intervals_datetimes.append(t[0])
+
+
+def perdelta(start, end, duration):
+    current = start
+    while current <= end:
+        yield current
+        current += duration
+
+
+stack = []
+for i, k in zip(intervals_datetimes[0::2], intervals_datetimes[1::2]):
+    for y in perdelta(i, k, timedelta(minutes=1)):
+        stack.append(y)
+
+
+minutes_list = [z.minute for z in stack]
+
+most_frequent_min = Counter(minutes_list).most_common(1)
+print(most_frequent_min[0][0] * most_min_guard)
